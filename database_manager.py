2323b8e94ffc2e0432d092b2b1e1880cc5cc10c1
jhonathadaniel <jhonathask8@hotmail.com>
2023-08-26 15:29:57 -0300

    bdV2

--- database_manager.py
+++ database_manager.py
@@ -6,17 +6,17 @@ class DatabaseManager:
         self.cursor = self.conn.cursor()
 
     def create_tables(self):
-        # Crie tabelas para ambientes, produtos e preços, se necessário
+        # Criar tabelas para ambientes, produtos e preços, se necessário
         pass
 
-    def add_environment(self, name, size, wifi_calibration_data):
-        # Adicione informações sobre o ambiente ao banco de dados
+    def add_environment(self, nome, size, wifi_calibration_data):
+        # Adicionar informações sobre o ambiente ao banco de dados
         pass
 
-    def add_product(self, name, price, position, image_path):
-        # Adicione informações sobre o produto ao banco de dados
+    def add_product(self, nome, preco, posicao, image_path):
+        # Adicionar informações sobre o produto ao banco de dados
         pass
 
-    def get_product_prices(self, product_name):
-        # Consulte o banco de dados para obter os preços do produto
+    def get_product_precos(self, nome_produto):
+        # Consultar o banco de dados para obter os preços do produto
         pass
