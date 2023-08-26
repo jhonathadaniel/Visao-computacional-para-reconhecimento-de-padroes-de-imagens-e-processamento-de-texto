c41ad3febabef38385f04bf1e6a7b0cc90e07639
jhonathadaniel <jhonathask8@hotmail.com>
2023-08-26 15:18:04 -0300

    indoorV2

--- indoor_recognition.py
+++ indoor_recognition.py
@@ -16,3 +16,13 @@ class indoor_location:
         # Usar intensidade do sinal Wi-Fi e informações calibradas para estimar a posição
         estimated_position = (x, y)  # Posição estimada
         return estimated_position
+
+    class MyTestCase(unittest.TestCase):
+        def test_something(self):
+            self.assertEqual(True, False)
+
+    if __name__ == '__main__':
+        unittest.main()
+
+    def indoor_location():
+        return None
