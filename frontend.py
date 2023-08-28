d0fc6673a35a77dd7f769f044b0bf0eda1068f80
jhonathadaniel <jhonathask8@hotmail.com>
2023-08-28 15:45:36 -0300

    layoutV1

---
+++ frontend.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+
+class Frontend:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sistema de Interação do Usuário")
+        self.root.geometry("400x300")
+
+        self.tabControl = ttk.Notebook(root)
+        self.tab1 = ttk.Frame(self.tabControl)
+        self.tab2 = ttk.Frame(self.tabControl)
+        self.tab3 = ttk.Frame(self.tabControl)
+        self.tab4 = ttk.Frame(self.tabControl)
+
+        self.tabControl.add(self.tab1, text="Processamento de Imagem e Texto")
+        self.tabControl.add(self.tab2, text="Localização Indoor com Wi-Fi")
+        self.tabControl.add(self.tab3, text="Identificação de Imagens via Câmera")
+        self.tabControl.add(self.tab4, text="Banco de Dados e Entrega")
+
+        self.tabControl.pack(expand=1, fill="both")
+
+        self.create_tab1_content()
+        self.create_tab2_content()
+        self.create_tab3_content()
+        self.create_tab4_content()
+
+    def create_tab1_content(self):
+        # Tab 1: Processamento de Imagem e Texto
+        label = tk.Label(self.tab1, text="Processamento de Imagem e Texto", font=("Helvetica", 12))
+        label.pack(pady=10)
+
+        # Implemente os elementos da interface para o processamento de imagem e texto
+
+    def create_tab2_content(self):
+        # Tab 2: Localização Indoor com Wi-Fi
+        label = tk.Label(self.tab2, text="Localização Indoor com Wi-Fi", font=("Helvetica", 12))
+        label.pack(pady=10)
+
+        # Implemente os elementos da interface para a localização indoor com Wi-Fi
+
+    def create_tab3_content(self):
+        # Tab 3: Identificação de Imagens via Câmera
+        label = tk.Label(self.tab3, text="Identificação de Imagens via Câmera", font=("Helvetica", 12))
+        label.pack(pady=10)
+
+        # Implemente os elementos da interface para a identificação de imagens via câmera
+
+    def create_tab4_content(self):
+        # Tab 4: Banco de Dados e Entrega
+        label = tk.Label(self.tab4, text="Banco de Dados e Entrega", font=("Helvetica", 12))
+        label.pack(pady=10)
+
+        # Implemente os elementos da interface para o banco de dados e sistema de entrega
+
+def main():
+    root = tk.Tk()
+    app = Frontend(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
