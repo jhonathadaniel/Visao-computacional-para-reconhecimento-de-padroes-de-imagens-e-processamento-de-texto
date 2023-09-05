11070cea716637cbf22ef1dffe1e116ba88398e2
Guilherme Henrique Fortes Trindade Filho <guilleinput@gmail.com>
2023-09-05 18:30:27 -0300

    Start do tensorflow
    Teste inicial para assertividade dos modelos.

--- image_recognition.py
+++ image_recognition.py
@@ -1,16 +1,35 @@
-import cv2
+import os
+import numpy as np
+import tensorflow as tf
 
-class ImageRecognitionSystem:
-    def __init__(self, model_path):
-        self.model = cv2.dnn.readNet(model_path)
+from tensorflow.keras.preprocessing.image import ImageDataGenarator
+from tensorflow.keras.models import Sequencial
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+#configs
+date_dir = './Data'
+img_height, img_width = 150, 150
+batch_size = 30
+num_classes = 2
+epochs = 25
+
+# gerador de dados
+
+train_datagem = ImageDataGenerator()
 
 
-    def identify_objects(self, image):
-        # Pré-processamento da imagem se necessário
-        blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size=(300, 300), swapRB=True)
-        self.model.setInput(blob)
-        output = self.model.forward()
 
-        # Processar a saída da rede para identificar objetos
-        identified_objects = []  # Lista de objetos identificados
-        return identified_objects
+# class ImageRecognitionSystem:
+#     def __init__(self, model_path):
+#         self.model = cv2.dnn.readNet(model_path)
+
+
+#     def identify_objects(self, image):
+#         # Pré-processamento da imagem se necessário
+#         blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size=(300, 300), swapRB=True)
+#         self.model.setInput(blob)
+#         output = self.model.forward()
+
+#         # Processar a saída da rede para identificar objetos
+#         identified_objects = []  # Lista de objetos identificados
+#         return identified_objects
